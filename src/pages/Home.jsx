fb42f8c6c61676e085542436bdb76999cd5cec12
erion321 <erionshehi800@gmail.com>
2023-01-26 23:30:27 +0100

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
         <Navbar />
         <Sidebar />
         <Products />
-        <section className="bg-white sticky bottom-0 left-0 w-full h-24 flex justify-center">
+        <section className="bg-white fixed bottom-0 left-0 w-full h-20 flex justify-center">
           <button
             className="absolute text-4xl top-[-1rem] bg-white shadow-lg hover:drop-shadow-lg hover:top-[-1.2rem] ease-in-out duration-200 shadow-gray-400 p-1 rounded-xl"
             onClick={() => navigate("/cart")}
