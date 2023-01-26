4bf855389f6fdf73d89b112c3e00228ccf096b99
erion321 <erionshehi800@gmail.com>
2023-01-26 23:16:12 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Item from "./pages/Item";
-import Cart from "./pages/cart";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
